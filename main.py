3b77b72758c8524768218a6619bfe4af5041ac2e
ukiwaman-inochi <146133313+ukiwaman-inochi@users.noreply.github.com>
2026-06-03 13:38:50 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import time
+import requests
+
+USER_KEY = os.environ["PUSHOVER_USER_KEY"]
+APP_TOKEN = os.environ["PUSHOVER_APP_TOKEN"]
+
+sent = False
+
+while True:
+    try:
+        r = requests.get(
+            "www.youtube.com/@HibachiMana/live",
+            allow_redirects=False,
+            timeout=20
+        )
+
+        live_now = r.status_code == 302
+
+        if live_now and not sent:
+            requests.post(
+                "api.pushover.net/1/messages.json",
+                data={
+                    "token": APP_TOKEN,
+                    "user": USER_KEY,
+                    "message": "緋八マナが配信開始しました",
+                    "title": "緋八マナライブ通知",
+                    "priority": 2,
+                    "retry": 60,
+                    "expire": 3600,
+                },
+            )
+            sent = True
+
+        if not live_now:
+            sent = False
+
+    except Exception as e:
+        print(e)
+
+    time.sleep(60)
